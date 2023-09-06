0bac4b82798a5b4751034e72c77dc7b6a0d4f9a6
LouisAndrew <61015467+LouisAndrew@users.noreply.github.com>
2023-09-06 14:12:24 +0200

    fix(event-bus): fix missing event declaration (#3)

--- utils/event-bus.ts
+++ utils/event-bus.ts
@@ -4,4 +4,5 @@ export const eventBus = new TinyEmitter();
 export const EVENTS = {
   UPDATE_INDEX_I: "UPDATE_INDEX_I",
   UPDATE_INDEX_J: "UPDATE_INDEX_J",
+  TERMINATE_SORT: "TERMINATE_SORT",
 } as const;
